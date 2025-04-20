7a62f1d092b204658b14cdf125e4a28779793306
BEBRIK18 <vinniamink@gmail.com>
2025-04-20 13:29:46 +0300

    game with 2 player and tennis ball

--- game.py
+++ game.py
@@ -18,16 +18,31 @@ class GameSprite(sprite.Sprite):
 
 class Player(GameSprite):
     def update(self):
+        if key_pressed[K_w] and self.rect.y > -50:
+            self.rect.y -= self.speed
+        if key_pressed[K_s] and self.rect.y < 350:
+            self.rect.y += self.speed
+
+class Player2(GameSprite):
+    def update(self):
         if key_pressed[K_UP] and self.rect.y > -50:
             self.rect.y -= self.speed
         if key_pressed[K_DOWN] and self.rect.y < 350:
             self.rect.y += self.speed
+
+class Tennis_Ball(GameSprite):
+    def update(self):
+        self.rect.x += self.speed
+        if self.rect.x >= 600:
+            self.rect.x = 150
 
 
 window = display.set_mode((700, 500))
 display.set_caption('Пинг-понг')
 
 player = Player('stick.png', -10, 50, 10, 70, 200)
+player2 = Player2('stick.png', 650, 50, 10, 70, 200)
+ball = Tennis_Ball('tennis.png', 200, 250, 5, 50, 50)
 
 
 FPS = 60
@@ -40,6 +55,8 @@ while game:
 
     if finish != True:
         player.update()
+        player2.update()
+        ball.update()
 
 
 
@@ -48,6 +65,8 @@ while game:
             game = False
 
     player.reset()
+    player2.reset()
+    ball.reset()
     clock.tick(FPS)
 
     display.update()    
